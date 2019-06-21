98c1f0dc930b2cab35367deeecedab3207abf1be
pascou <anhe-pascal.anhe.1@ens.etsmtl.ca>
2019-06-21 11:40:22 -0400

    partie jusqua liste atttendee

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,4 +1,9 @@
-import React ,{Component} from 'react';
+import React ,{Component,Fragment} from 'react';
+//import { Button } from 'semantic-ui-react';
+
+import EventDashboard from '../../features/event/EventDashbooard/EventDasboard'
+import NavBar from '../../features/nav/NavBar/NavBar';
+import { Container } from 'semantic-ui-react';
 
 
 
@@ -6,9 +11,14 @@ class App extends Component {
   
   render(){
     return (
-      <div>
-        <h1>Re-vents</h1>
-      </div>
+      <Fragment>
+        <NavBar/>
+
+        <Container className="main">
+        <EventDashboard/>
+        </Container>
+       
+      </Fragment>
     );
   }
   
